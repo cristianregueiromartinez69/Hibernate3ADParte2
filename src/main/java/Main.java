12b68d261064dca27ccf87eed9be6332d6e2fea7
cristian <cregueiromartinez@danielcastelao.org>
2024-11-27 09:51:12 +0100

    3 metodos hechos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+import service.Crud;
+import service.MetodosAdestrador;
+import service.MetodosPokedex;
+
 public class Main {
 
     public static void main(String[] args) {
+        MetodosPokedex metodosPokedex = new MetodosPokedex();
+        MetodosAdestrador metodosAdestrador = new MetodosAdestrador();
+        Crud crud = new Crud();
+        //crud.insertar10Pokemons(metodosPokedex.getPokemonsList());
+        //crud.insertar2Adestradores(metodosAdestrador.getAdestradoresList());
+        crud.readPokemonsFromPokedex();
 
     }
 }
--- src/main/java/service/Crud.java
+++ src/main/java/service/Crud.java
@@ -1,6 +1,66 @@
 package service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import model.Adestrador;
+import model.Pokedex;
+
+import java.util.List;
+
 public class Crud {
 
+    public void insertar10Pokemons(List<Pokedex> pokedexList){
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager em = managerFactory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try(managerFactory; em){
+            tx.begin();
+
+            for(Pokedex pokedex : pokedexList){
+                em.persist(pokedex);
+            }
+            System.out.println("Los 10 pokemons han sido insertados con éxito");
+            tx.commit();
+        }finally{
+            if(tx.isActive()) tx.rollback();
+        }
+    }
+
+    public void insertar2Adestradores(List<Adestrador> adestradorList){
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager em = managerFactory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try(managerFactory; em){
+            tx.begin();
+
+            for(Adestrador adestrador : adestradorList){
+                em.persist(adestrador);
+            }
+            System.out.println("Los 2 adestradores han sido ");
+            tx.commit();
+        }
+    }
+
+    public void readPokemonsFromPokedex(){
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager em = managerFactory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        List<Pokedex> pokedexList = null;
+        try(managerFactory; em){
+            tx.begin();
+            pokedexList = em.createQuery("SELECT p FROM Pokedex p", Pokedex.class).getResultList();
+
+            for(Pokedex pokedex : pokedexList){
+                System.out.println(pokedex);
+            }
+            tx.commit();
+        }finally{
+            if(tx.isActive()) tx.rollback();
+        }
+    }
 
 }
